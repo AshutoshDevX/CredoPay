785ab27a341b81bd692a672934ad17f523d8c8af
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-07-04 06:14:04 +0530

    Add routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-
+import User from './routes/user.js'
+import Transactions from './routes/transaction.js'
 const app = express();
 
 
@@ -11,7 +12,8 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-
+app.use("/user", User);
+app.use("/transactions", Transactions)
 
 app.listen(PORT, () => {
     console.log(`server running at ${3000}`)
