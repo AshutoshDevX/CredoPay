eb0c1a4ab64bfd0a14049ba22ea5e5bf854681f1
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-07-04 18:25:31 +0530

    Add routes and update routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
-import { Home } from "./pages/Home"
-import { BrowserRouter, Routes, Route } from "react-router"
-import { SignIn } from "./pages/SignIn"
-import { SignUp } from "./pages/SignUp"
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Toaster } from "@/components/ui/sonner";
+import { CustomerTransactions } from "./Pages/CustomerTransaction";
+import { BankerAccounts } from "./Pages/BankerAccounts";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/transactions" element={<CustomerTransactions />} />
+        <Route path="/banker" element={<BankerAccounts />} />
       </Routes>
+      <Toaster richColors position="top-center" />
     </BrowserRouter>
-  )
+  );
 }
-export default App
 
-
+export default App;
