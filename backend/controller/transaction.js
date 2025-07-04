8f71c24162a7dbc44df04ebc1ee0b2eb3f2bb9f1
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-07-04 05:52:47 +0530

    Add transactions logic

---
+++ backend/controller/transaction.js
@@ -0,0 +1,77 @@
+import { PrismaClient } from '../generated/prisma/index.js';
+const prisma = new PrismaClient()
+
+export const getTransactions = async (req, res) => {
+    const accounts = await prisma.account.findMany({
+        where: { user_id: req.user.user_id },
+        include: { transactions: true }
+    });
+
+    res.json(accounts);
+}
+
+
+export const deposit = async (req, res) => {
+    const { account_id, amount } = req.body;
+
+    if (amount <= 0) return res.status(400).json({ message: 'Invalid deposit amount' });
+
+    const account = await prisma.account.findFirst({
+        where: { account_id, user_id: req.user.user_id }
+    });
+
+    if (!account) return res.status(404).json({ message: 'Account not found' });
+
+    await prisma.$transaction([
+        prisma.transaction.create({
+            data: {
+                account_id,
+                type: 'deposit',
+                amount
+            }
+        }),
+        prisma.account.update({
+            where: { account_id },
+            data: {
+                balance: { increment: amount }
+            }
+        })
+    ]);
+
+    res.json({ message: 'Deposit successful' });
+}
+
+
+export const withdraw = async (req, res) => {
+    const { account_id, amount } = req.body;
+
+    if (amount <= 0) return res.status(400).json({ message: 'Invalid withdrawal amount' });
+
+    const account = await prisma.account.findFirst({
+        where: { account_id, user_id: req.user.user_id }
+    });
+
+    if (!account) return res.status(404).json({ message: 'Account not found' });
+
+    if (account.balance < amount) {
+        return res.status(400).json({ message: 'Insufficient Funds' });
+    }
+
+    await prisma.$transaction([
+        prisma.transaction.create({
+            data: {
+                account_id,
+                type: 'withdrawal',
+                amount
+            }
+        }),
+        prisma.account.update({
+            where: { account_id },
+            data: {
+                balance: { decrement: amount }
+            }
+        })
+    ]);
+
+    res.json({ message: 'Withdrawal successful' });
+}
