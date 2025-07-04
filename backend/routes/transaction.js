8b26255cd430358983281891ed6118aa2390f70a
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-07-04 05:52:31 +0530

    Add transaction routes

---
+++ backend/routes/transaction.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { authMiddleware } from '../Auth/auth.js';
+import { deposit } from '../controller/transaction.js';
+import { getTransactions } from '../controller/transaction.js';
+import { withdraw } from '../controller/transaction.js';
+const router = express.Router();
+
+router.get("/transactions", authMiddleware, getTransactions);
+router.post("/deposit", authMiddleware, deposit);
+router.post("/withdraw", authMiddleware, withdraw);
+
+
+export default router;
