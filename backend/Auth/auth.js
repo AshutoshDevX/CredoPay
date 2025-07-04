f7b0a6f703e4f35c2c775fe6cae7fb6256603907
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-07-04 05:34:04 +0530

    Add auth middleware

---
+++ backend/Auth/auth.js
@@ -0,0 +1,10 @@
+const authMiddleware = async (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: 'No token provided' });
+
+    const user = await prisma.user.findFirst({ where: { access_token: token } });
+    if (!user) return res.status(403).json({ message: 'Invalid token' });
+
+    req.user = user;
+    next();
+};
