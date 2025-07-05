a159e53a9f807eef52fe5da9f968d2c04ecae118
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-07-05 13:28:33 +0530

    update signin/signup page layout

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 export const SignUp = () => {
     return (
         <div className="flex h-screen w-full items-center justify-center p-6 md:p-10 bg-[#0a0f3c]">
-            <div className="absolute w-[700px] h-[700px] bg-blue-600 rounded-full blur-[150px] opacity-30 top-10 left-20 z-0"></div>
+            <div className="absolute w-[700px] h-[700px] max-w-[100vw] bg-blue-600 rounded-full blur-[150px] opacity-30 top-10 left-1/2 -translate-x-1/2 z-0"></div>
             <Link to="/"><h1 className="absolute top-10 left-10 text-4xl text-white">CredoPay</h1></Link>
             <div className="w-full max-w-sm">
                 <SignUpForm />
